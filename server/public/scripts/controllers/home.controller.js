88837f9fd87a8cb4b9edb93fcffe698872be82c6
Sam Bahta <smabahta37@gmail.com>
2018-08-24 14:12:30 -0500

    add new pet inputs

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -2,4 +2,16 @@ hotelApp.controller('HomeController', ['$http', function($http){
     vm = this;
     console.log('HomeController Loaded');
     
+    vm.addNewPet = function (){
+        let newPet ={
+            name:vm.nameIn,
+            type:vm.typeIn,
+            description:vm.notesIn,
+            checked_in:vm.checkIn,
+            owner_id:vm.ownerPin,
+        }
+        console.log(newPet);
+        
+    }
+
 }]); //end home controller
