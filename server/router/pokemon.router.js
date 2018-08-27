efb14a9003a93295bbd4a959c8e013b0fe8bbec3
Sam Bahta <smabahta37@gmail.com>
2018-08-26 19:24:54 -0500

    pokemon selection and targeting functional

--- server/router/pokemon.router.js
+++ server/router/pokemon.router.js
@@ -14,11 +14,33 @@ router.get('/', (req,res)=>{
 });
 router.get('/kanto', (req,res)=>{
     console.log('GET Route');
-    const query = `SELECT * From "pokemon" WHERE "region" like 'Kanto';`;
+    const query = `SELECT * From "pokemon" WHERE "region" like 'Kanto' ORDER BY "poke_id" ASC;`;
     pool.query(query).then(results=>{
         res.send(results.rows);
     }).catch((error)=>{
         console.log('Error GETting by Kanto', error);
+        res.sendStatus(500);
+    });
+});
+
+router.get('/johto', (req,res)=>{
+    console.log('GET Route');
+    const query = `SELECT * From "pokemon" WHERE "region" like 'Johto' ORDER BY "poke_id" ASC;`;
+    pool.query(query).then(results=>{
+        res.send(results.rows);
+    }).catch((error)=>{
+        console.log('Error GETting by johto', error);
+        res.sendStatus(500);
+    });
+});
+
+router.get('/hoenn', (req,res)=>{
+    console.log('GET Route');
+    const query = `SELECT * From "pokemon" WHERE "region" like 'Hoenn' ORDER BY "poke_id" ASC;`;
+    pool.query(query).then(results=>{
+        res.send(results.rows);
+    }).catch((error)=>{
+        console.log('Error GETting by hoenn', error);
         res.sendStatus(500);
     });
 });
